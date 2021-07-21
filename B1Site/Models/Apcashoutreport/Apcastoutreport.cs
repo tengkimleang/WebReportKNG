22b423954fa47011bdb89d70a10f23ed00367698
Sountitya <sountitya99@gmail.com>
2021-07-21 14:00:36 +0700

    ApreportCashout

---
+++ B1Site/Models/Apcashoutreport/Apcastoutreport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B1Site.Models.Apcashoutreport
+{
+    public class Apcastoutreport
+    {
+        public string Apdocnum { get; set; }
+        public string DocumentDate { get; set; }
+
+        public string PaymentDoc { get; set; }
+        public string PaidDate { get; set; }
+        public string VendorName { get; set; }
+        public string NameVendorissueCheck { get; set; }
+        public string InternalDocument { get; set; }
+        public string ExternalDocument { get; set; }
+        public string Glaccount { get; set; }
+        public string GlaccountName { get; set; }
+        public string Decription { get; set; }
+        public string Department { get; set; }
+        public string Branch { get; set; }
+        public string ProductName { get; set; }
+        public string PaymentMethod { get; set; }
+        public string CheckNo { get; set; }
+        public string BankName { get; set; }
+        public string Cash { get; set; }
+        public string Debit { get; set; }
+        public string Credit { get; set; }
+    }
+}
---
+++ B1Site/Service/IApcashoutService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B1Site.Service
+{
+  public  interface IApcashoutService
+    {
+        //Task<List<CustomerIDMaster>> GetCustomerIDMastersAsync();
+        Task<string> GetApcastoutAsync(DateTime datefrom, DateTime dateto);
+    }
+}
