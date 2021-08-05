e4d2caefa7421bc62c0b1d0bbe2b735a92f50893
Sountitya <sountitya99@gmail.com>
2021-08-05 16:45:46 +0700

    FinaceSaleReport

---
+++ B1Site/Models/FinalSaleReport/CategoryMaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B1Site.Models.FinalSaleReport
+{
+    public class CategoryMaster
+    {
+        public string Category { get; set; }
+    }
+}
---
+++ B1Site/Models/FinalSaleReport/ItemsGropMaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B1Site.Models.FinalSaleReport
+{
+    public class ItemsGropMaster
+    {
+        public string ItemsGroupCode { get; set; }
+        public string ItemsGoupName { get; set; }
+    }
+}
