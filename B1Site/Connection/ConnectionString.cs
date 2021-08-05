31fa9180165f239f3600efd4ba2396dbabd2b422
tengkimleang1 <nonametotell3@gmail.com>
2021-08-05 09:06:24 +0700

    Add New Feature of in Startup Page and Modify some coding and Create View Administrator

--- B1Site/Connection/ConnectionString.cs
+++ B1Site/Connection/ConnectionString.cs
@@ -7,8 +7,19 @@ namespace B1Site.Connection
 {
     public class ConnectionString
     {
+        public ConnectionString(string ConSTRWeb,string ConSTRDB,string DataSources,string userName,string passWord)
+        {
+            constrWeb = ConSTRWeb;
+            constrDb = ConSTRDB;
+            DataSource = DataSources;
+            UserName = userName;
+            PassWord = passWord;
+        }
         public static string constr { get; set; }
         public static string constrWeb { get; set; }
         public static string constrDb { get; set; }
+        public static string DataSource { get; set; }
+        public static string UserName { get; set; }
+        public static string PassWord { get; set; }
     }
 }
